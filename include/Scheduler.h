3bb8989dd633f38ec458b0269c5293c138b5906d
Muhammad Saad Khan <mksaad2006@gmail.com>
2026-05-10 16:47:25 +0500

    Added basic Schedular class skeleton

---
+++ include/Scheduler.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include "Section.h"
+#include "Venue.h"
+
+
+class Scheduler{
+    vector<Venue*> venues;
+    vector <Section*> sections;
+    public:
+    Scheduler(vector<Venue*> v,vector <Section*> s){
+        venues=v;
+        sections=s;
+    }
+    bool isConflict(string venueID,string timeSlot);
+    void displaySchedule();
+    bool checkCapacity(Venue*v,int numstudents);
+}
